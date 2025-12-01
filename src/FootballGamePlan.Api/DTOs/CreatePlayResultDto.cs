8151790ce391101af60dab6b1500239f15ab977f
Cristoffer Dessezar <dessezar@outlook.com>
2025-12-01 15:05:28 +0100

    added CreatePlayResultDto

---
+++ src/FootballGamePlan.Api/DTOs/CreatePlayResultDto.cs
@@ -0,0 +1,14 @@
+﻿using FootballGamePlan.Api.Models;
+
+namespace FootballGamePlan.Api;
+
+public class CreatePlayResultDto
+{
+    public int GamePlanId { get; set; }
+    public int PlayId { get; set; }
+    public Coverage Coverage { get; set; }
+    public PassFailReason PassFailReason { get; set; }
+    public bool IsPass { get; set; }
+    public int YardsGained { get; set; }
+
+}
