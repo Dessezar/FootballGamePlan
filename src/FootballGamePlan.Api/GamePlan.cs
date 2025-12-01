f8e634923fbb3c0c82d30bf80dd19f6dd4d78a28
Cristoffer Dessezar <dessezar@outlook.com>
2025-12-01 13:18:50 +0100

    added GamePlan class

--- src/FootballGamePlan.Api/GamePlan.cs
+++ src/FootballGamePlan.Api/GamePlan.cs
@@ -1,9 +1,9 @@
 ﻿namespace FootballGamePlan.Api;
 
-public class Play
+public class GamePlan
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public bool IsPass { get; set; }
     public List<PlayResult> Results { get; set; } = new();
+    public List<Play> Plays { get; set; } = new();
 }
--- src/FootballGamePlan.Api/PassFailReason.cs
+++ src/FootballGamePlan.Api/PassFailReason.cs
@@ -5,5 +5,4 @@ public enum PassFailReason
     Drop = 0,
     BadThrow = 1,
     GoodCoverage = 2
-
 }
