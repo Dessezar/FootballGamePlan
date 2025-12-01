05e9cbe91a5db9a7d7389bb34de961ee8bfa4bb9
Cristoffer Dessezar <dessezar@outlook.com>
2025-12-01 14:12:27 +0100

    Created DbContext

---
+++ src/FootballGamePlan.Api/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FootballGamePlan.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballGamePlan.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<GamePlan> GamePlans => Set<GamePlan>();
+    public DbSet<Play> Plays => Set<Play>();
+    public DbSet<PlayResult> PlayResults => Set<PlayResult>();
+
+    
+    
+
+}
