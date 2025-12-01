2993a3d363393c07af54beee390146786f4f3d34
Cristoffer Dessezar <dessezar@outlook.com>
2025-12-01 13:15:09 +0100

    Added play class

---
+++ src/FootballGamePlan.Api/Play.cs
@@ -0,0 +1,9 @@
+﻿namespace FootballGamePlan.Api;
+
+public class Play
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsPass { get; set; }
+    public List<PlayResult> Result { get; set; } = new();
+}
